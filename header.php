0cd1ac149acaec71b5232759b04f459172b9ba90
Rainer Karcher <rainerkarcher@Rainers-MacBook-Pro.local>
2021-03-21 11:49:21 -0500

    Swapped side nav items around; scrolling still broken on mobile devices

--- header.php
+++ header.php
@@ -77,24 +77,6 @@ img {
             </a>
           </li>
         <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Strategy</span>
-          <a class="link-secondary" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>    
-          <li class="nav-item">
-            <a class="nav-link" href="mementos.php">
-              <span data-feather="bar-chart-2"></span>
-              Mementos & Requests
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="calc/index.html#/list">
-              <span data-feather="shopping-cart"></span>
-              Persona Fusion Calculator
-            </a>
-          </li>
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
           <span>Social</span>
           <a class="link-secondary" href="#" aria-label="Add a new report">
             <span data-feather="plus-circle"></span>
@@ -159,7 +141,26 @@ img {
               <span data-feather="layers"></span>
               Video Games
             </a>
-          </li> 
+          </li>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Strategy</span>
+          <a class="link-secondary" href="#" aria-label="Add a new report">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+          <li class="nav-item">
+            <a class="nav-link" href="calc/index.html#/list">
+              <span data-feather="shopping-cart"></span>
+              Persona Fusion Calculator
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="mementos.php">
+              <span data-feather="bar-chart-2"></span>
+              Mementos & Requests
+            </a>
+          </li>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"> 
         </ul>
       </div>
     </nav>
